df09a250b1049c64cbf308ef148ef1ba5cdf6a0c
Fanny <mardysh@gmail.com>
2018-04-05 16:45:15 +0200

    Correction login

--- login.php
+++ login.php
@@ -1,29 +1,32 @@
-<?php include('layout/head.php') ?>
-<?php include('server.php') ?>
-</head>
+<?php
+$title = "Accueil";
+$description = "Ma description";
+?>
 
-<body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-	 
+<?php include_once("php/db.php"); ?>
+<?php include_once("layout/head.php"); ?>
+<?php include_once("server.php"); ?>
+
+<div class="header">
+    <h2>Login</h2>
+</div>
   <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php" style="color:red">Sign up</a>
-  	</p>
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username" >
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="login_user">Login</button>
+    </div>
+    <p>
+      Not yet a member? <a href="register.php" style="color:red">Sign up</a>
+    </p>
   </form>
-</body>
-<?php include 'layout/footer.php';?>
-</html>
+
+
+<?php include_once("layout/footer.php"); ?>
