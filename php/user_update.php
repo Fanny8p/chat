1830071f257de8c2e9c379cef8887ebe1171409c
Fanny <mardysh@gmail.com>
2018-04-09 09:55:41 +0200

    ///

--- php/user_update.php
+++ php/user_update.php
@@ -9,31 +9,21 @@ include('db.php');
 include('get_auth_user.php');
 
 //GET FORM VALUES
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$birthday = $_POST['birthday'];
-$city = $_POST['city'];
 $pseudo = $_POST['name'];
+$birthday = $_POST['birthday'];
 $avatar = $_FILES['avatar']['name'];
-
 
 //MOVE IMAGE TO SERVER FOLDER
 move_uploaded_file($_FILES['avatar']['tmp_name'], "../image/$name");
 
 //UPDATE USER INFORMATIONS
-$stmt = $dbh->prepare('UPDATE users SET  
-	firstname = :firstname,
-	lastaname = :lastaname, 
-	birthday = :birthday,
-	city = 	:city,  
-	username = :pseudo,
-	image = :avatar
+$stmt = $dbh->prepare('UPDATE users SET 
+	username = :pseudo, 
+	birthday = :birthday, 
+	image = :avatar 
  WHERE id ='. $id);
-$stmt->bindParam(":firstname", 	$firstname); 
-$stmt->bindParam(":lastname", 	$lastname); 
-$stmt->bindParam(":birthday", 	$birthday);
-$stmt->bindParam(":city", 		$city); 
 $stmt->bindParam(":pseudo", 	$pseudo);
+$stmt->bindParam(":birthday", 	$birthday); 
 $stmt->bindParam(":avatar", 	$avatar);
 $result = $stmt->execute();
 
