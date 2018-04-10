b591abb5c591d4f519408cf1206b031f6b6b952c
Unknown <marion.moriceau3@gmail.com>
2018-04-10 10:54:08 +0200

    Refresh is bac k for now

--- php/refresh.php
+++ php/refresh.php
@@ -5,22 +5,24 @@
 include ("db.php");
  
 // on crée la requête SQL 
-$sql = "SELECT message.message, message.time, users.username FROM message JOIN users ON users.id = message.user_id WHERE chatroom_id= :id')"; 
+$sql = "SELECT message.message, message.time, users.username FROM message JOIN users ON users.id = message.user_id WHERE chatroom_id=:chatroom_id"; 
 
-$stmt= $dbh->prepare($query);
-$stmt->bindParam(":id", $id);
-$sth->execute();
-
+$stmt= $dbh->prepare($sql);
+$stmt->bindParam(":chatroom_id", $chatroom_id);
+$stmt->execute();
+$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // on fait une boucle qui va faire un tour pour chaque enregistrement 
-while($data = mysqli_fetch_assoc($req)) 
-    {	$emoji_replace = array(':)',':-)','(angry)',':3',":'(",':|',':(',':-(', ';)',';-)');
-		$emoji_new = array('<img src ="emojis/emo_smile.png"/>','<img src ="emojis/emo_smile.png"/>','<img src ="emojis/emo_angry.png"/>','<img src ="emojis/emo_cat.png"/>','<img src ="emojis/emo_cry.png"/>','<img src ="emojis/emo_noreaction.png"/>','<img src ="emojis/emo_sad.png"/>','<img src ="emojis/emo_sad.png"/>','<img src ="emojis/emo_wink.png"/>','<img src ="emojis/emo_wink.png"/>');
-		$data['message'] = str_replace($emoji_replace, $emoji_new, $data['message']);
-    // on affiche les informations de l'enregistrement en cours 
-    echo'<div>'.$data['time'].'</div>'; 
-    echo'<div>'.$data['username'].'</div>';
-    echo'<div><b>'.$data['message'].'</b></div>';
-    } 
-
-?> 
+foreach ($messages as $message) { ?>
+  
+  <?php $emoji_replace = array(':)',':-)','(angry)',':3',":'(",':|',':(',':-(', ';)',';-)');
+        $emoji_new = array('<img src ="emojis/emo_smile.png"/>','<img src ="emojis/emo_smile.png"/>','<img src ="emojis/emo_angry.png"/>','<img src ="emojis/emo_cat.png"/>','<img src ="emojis/emo_cry.png"/>','<img src ="emojis/emo_noreaction.png"/>','<img src ="emojis/emo_sad.png"/>','<img src ="emojis/emo_sad.png"/>','<img src ="emojis/emo_wink.png"/>','<img src ="emojis/emo_wink.png"/>');
+        $message['message'] = str_replace($emoji_replace, $emoji_new, $message['message']); ?>
+  
+  
+      <li>
+        <small>écrit par <?php echo $message['username']; ?> a <?php echo $message['time']; ?></small>
+        <p><?php echo $message['message']; ?></p>
+        </li>
+        <hr>
+    <?php }
