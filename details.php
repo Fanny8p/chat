9140883aeb15fe757f5da9136f5e1f5587ea637b
Fanny <mardysh@gmail.com>
2018-04-10 10:56:05 +0200

    Addind css details

--- details.php
+++ details.php
@@ -11,13 +11,19 @@ $description = "Details of chatroom";
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-12 col-lg-8 col-xl-6 text-center">
-        <h1>Chatroom : <?php echo $chatroom['name']; ?></h1>
+        <div class="head_details">
+          <h1>Room</h1>
+          <h1 style="color: #00dfa8;">"<?php echo $chatroom['name']; ?>"</h1>
+          <hr>
+        </div>
   			<textarea class="form-control" aria-label="With textarea" value="<?php echo $chatroom['description']; ?>" placeholder="Chatroom's description" name="description" id="description" required></textarea>
+        <img src="/chat/image/default.jpg" class="rounded-circle" alt="Avatar de <?php echo $user['username']; ?>">
+
         <p class="text-h3 mt-5">		
         	<div class="form-group">
-		    <input class="btn btn-primary" type="submit">
-			</div>
-		</p>
+		        <input class="btn btn-primary" type="submit">
+			    </div>
+		    </p>
       </div>
     </div>
   </div>
--- php/user_update.php
+++ php/user_update.php
@@ -9,34 +9,37 @@ include('db.php');
 include('get_auth_user.php');
 
 //GET FORM VALUES
+$avatar = $_FILES['avatar']['name'];
 $pseudo = $_POST['name'];
 $birthday = $_POST['birthday'];
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
 $city = $_POST['city'];
 $zipcode = $_POST['zipcode'];
-$avatar = $_FILES['avatar']['name'];
+
 
 //MOVE IMAGE TO SERVER FOLDER
 move_uploaded_file($_FILES['avatar']['tmp_name'], "/chat/image/$name");
 
 //UPDATE USER INFORMATIONS
 $stmt = $dbh->prepare('UPDATE users SET 
+	image = :avatar, 
 	username = :pseudo, 
 	birthday = :birthday,
 	firstname = :firstname,
 	lastname = :lastname,	
 	city = :city, 
-	zipcode = :zipcode, 
-	image = :avatar 
+	zipcode = :zipcode 
+
  WHERE id ='. $id);
+$stmt->bindParam(":avatar", 	$avatar);
 $stmt->bindParam(":pseudo", 	$pseudo);
 $stmt->bindParam(":birthday", 	$birthday); 
 $stmt->bindParam(":firstname", 	$firstname);
 $stmt->bindParam(":lastname", 	$lastname); 
 $stmt->bindParam(":city", 	$city); 
 $stmt->bindParam(":zipcode", 	$zipcode); 
-$stmt->bindParam(":avatar", 	$avatar);
+
 $result = $stmt->execute();
 
 //REDIRECT TO PROFIL
